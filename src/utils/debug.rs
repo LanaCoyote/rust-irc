1c78854176720dc1413046dcfa2ddfc50a3511e5
David Reeve <nerdboy6@gmail.com>
2015-01-20 11:45:28 -0500

    Genericized the debug functions
    All debug functions now accept any string, not just string slices.

--- src/utils/debug.rs
+++ src/utils/debug.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub enum Level {
   Err,
   Warn,
@@ -8,36 +10,36 @@ pub enum Level {
 }
 
 #[allow(dead_code)]
-pub fn err ( s : &str, e : &str ) {
+pub fn err <T,S> ( s : T, e : S ) where T : fmt::String, S : fmt::String {
   let data = format! ( "err in {} : {}", s, e );
   log( Level::Err, data );
 }
 
 #[allow(dead_code)]
-pub fn warn ( s : &str, w : &str ) {
+pub fn warn <T,S> ( s : T, w : S ) where T : fmt::String, S : fmt::String {
   let data = format! ( "warning in {} : {}", s, w );
   log( Level::Warn, data );
 }
 
 #[allow(dead_code)]
-pub fn oper ( s : &str ) {
-  log( Level::Oper, s.to_string( ) );
+pub fn oper <T> ( s : T ) where T : fmt::String {
+  log( Level::Oper, s );
 }
 
 #[allow(dead_code)]
-pub fn info ( s : &str ) {
-  log( Level::Info, s.to_string( ) );
+pub fn info <T> ( s : T ) where T : fmt::String {
+  log( Level::Info, s );
 }
 
 #[allow(dead_code)]
-pub fn disp ( s : &str, inc : bool ) {
+pub fn disp <T> ( s : T, inc : bool ) where T : fmt::String {
   match inc {
-    true  => log( Level::DispIn, s.to_string( ) ),
-    false => log( Level::DispOut, s.to_string( ) ),
+    true  => log( Level::DispIn, s ),
+    false => log( Level::DispOut, s ),
   }
 }
 
-fn log ( l : Level, s : String ) {
+fn log <T> ( l : Level, s : T ) where T : fmt::String {
   match l {
     Level::Err     => println!( "!! {}", s ),
     Level::Warn    => println!( "** {}", s ),
