8e92b690614ff18ab4adbbefb1a736097b90a9ee
David Reeve <nerdboy6@gmail.com>
2015-01-21 08:58:36 -0500

    Neatened reader.rs
    Broke start() down into multiple functions, removed magic numbers in
    favor of constants, added extensive comments for imports, etc.

--- src/reader.rs
+++ src/reader.rs
@@ -1,9 +1,17 @@
-use std::io;
-use std::sync::mpsc;
-use std::time;
+use std::io;                  // io for timers and TcpStreams
+use std::sync::mpsc::Sender;  // sender channel for passing back data
+use std::time::Duration;      // used for the sleep timer
 
-use connection;
-use utils::debug;
+// import custom modules
+use connection::ConnEvent;    // used for passing back messages to the client
+use utils::debug;             // output debug for logging errors
+
+static IRC_TRY_INITIAL  : u8    = 0;  // initial number of tries attempted
+static IRC_TRY_SUCCESS  : u8    = 0;  // set the try value to this on success
+static IRC_TRY_FAILURE  : u8    = 1;  // try increment on failure
+static IRC_TRY_LIMIT    : u8    = 5;  // maximum number of tries before failing
+static IRC_READ_TIMEOUT : usize = 5;  // initial time between irc reads
+static IRC_READ_MULT    : usize = 2;  // multiply timeout by this every failure
 
 /// `IrcReader` handles reading from an IRC stream
 ///
@@ -13,7 +21,7 @@ use utils::debug;
 /// `chan` - Send half of the channel used to communicate
 pub struct IrcReader {
   tcp   : io::TcpStream,
-  chan  : mpsc::Sender < connection::ConnEvent >,
+  chan  : Sender < ConnEvent >,
 }
 
 impl IrcReader {
@@ -25,11 +33,131 @@ impl IrcReader {
   /// `tx` - Transmission channel used to talk to the program
   pub fn new ( 
     tcp : io::TcpStream,
-    tx : mpsc::Sender < connection::ConnEvent > 
+    tx : Sender < ConnEvent > 
     ) -> IrcReader {
     IrcReader {
       tcp   : tcp,
       chan  : tx,
+    }
+  }
+  
+  /// `has_peer` returns whether the TCP connection has a valid peer
+  ///
+  /// # Returns
+  ///
+  /// true if the connection is valid, false otherwise.
+  fn has_peer ( &self ) -> bool {
+    // get the peer name from our tcp connection
+    match self.tcp.peer_name() {
+      // we did it, we're connected properly
+      Ok ( peer ) => {
+        debug::oper( format! ( "irc reader opened at {} successfully", peer ) );
+        true
+      },
+      
+      // couldn't connect for some reason
+      Err ( e )   => {
+        debug::err( "opening irc reader", e.desc );
+        match e.detail {
+          Some ( det ) => debug::info( det.as_slice( ) ),
+          None         => (),
+        };
+        false
+      },
+    }
+  }
+  
+  /// `handle_read_success` parses and sends back a line read from IRC
+  ///
+  /// # Arguments
+  ///
+  /// * `line` - the line read from the TcpStream
+  /// * `try` - the attempt number
+  ///
+  /// # Returns
+  ///
+  /// IRC_TRY_SUCCESS if the operation completed successfully. Otherwise it
+  /// returns try plus IRC_TRY_FAILURE.
+  fn handle_read_success ( &self, line : String, try : u8 ) -> u8 {
+    // trim whitespace (including the newline)
+    let pass = line.as_slice( ).trim_right( );
+    
+    // pass our message back to the client
+    match self.chan.send( ConnEvent::Recv( pass ) ) {
+      // it worked, reset the try counter
+      Ok ( _ )  => {
+        if try > 0 {
+          debug::info( format! ( "successful read after {} attempts", try ) );
+        }
+        IRC_TRY_SUCCESS
+      },
+      
+      // an error occurred
+      Err ( e ) => {
+        debug::err( "irc reader send", e.desc );
+        try + IRC_TRY_FAILURE
+      },
+    }
+  }
+  
+  /// `handle_read_failure` reports an error that occurred while reading
+  ///
+  /// # Arguments
+  ///
+  /// * `e` - the error returned by the TcpStream
+  /// * `try` - the attempt number
+  ///
+  /// # Returns
+  ///
+  /// IRC_TRY_LIMIT if the error is an EOF. Otherwise it increments try by
+  /// IRC_TRY_FAILURE.
+  fn handle_read_failure ( &self, e : io::IoError, try : u8 ) -> u8 {
+    match e.kind {
+      // eof means the tcp connection was closed
+      io::IoErrorKind::EndOfFile => {
+        debug::err( "irc reader receive", "eof reported, closing connection" );
+        IRC_TRY_LIMIT
+      },
+      
+      // all other errors
+      _                          => {
+        debug::err( "irc reader receive", e.desc );
+        match e.detail {
+          Some ( det ) => debug::info( det.as_slice( ) ),
+          None         => (),
+        };
+        try + IRC_TRY_FAILURE
+      },
+    }
+  }
+  
+  /// `get_next_try` gets whether the loop should continue
+  ///
+  /// # Arguments
+  ///
+  /// * `try` - the attempt number
+  /// * `time` - the timeout between attempts
+  ///
+  /// # Returns
+  ///
+  /// None if the reader has attempted too many reads without success. If the
+  /// read failed, it sleeps and returns the timeout times IRC_READ_MULT. If the
+  /// read succeeded it returns IRC_READ_TIMEOUT.
+  fn get_next_try ( &self, try : u8, time : usize ) -> Option < usize > {
+    // end the reader if we've gone over the try limit
+    if try >= IRC_TRY_LIMIT {
+      debug::err( "irc reader", format! ( "failed after {} retries", try ) );
+      None
+      
+    // if we've failed, don't retry immediately
+    } else if try > IRC_TRY_SUCCESS {
+      debug::info( format! ( "retrying after {} seconds...", time ) );
+      io::timer::sleep( Duration::seconds( time ) );
+      Some( time * IRC_READ_MULT )
+      
+    // we did it, reset the timer
+    } else if try == IRC_TRY_SUCCESS {
+      Some( IRC_READ_TIMEOUT )
     }
   }
   
@@ -40,76 +168,30 @@ impl IrcReader {
   /// - This will block until the connection is closed. Run it in a new thread
   /// so that you can operate on the information you read.
   pub fn start ( &mut self ) {
-    let mut try   = 0u8;
-    let mut time  = 5;
+    let mut try   = IRC_TRY_INITIAL;
+    let mut time  = IRC_READ_TIMEOUT;
     let mut read  = io::BufferedReader::new( self.tcp.clone( ) );
     // Check that we're connected to a peer
-    match self.tcp.peer_name() {
-      Ok ( peer ) => {
-        let out = format! ("opening irc reader at {}...", peer );
-        debug::oper( out.as_slice( ) );
-      },
-      Err ( e )   => {
-        debug::err( "opening irc reader", e.desc );
-        match e.detail {
-          Some( det ) => debug::info( det.as_slice( ) ),
-          None        => (),
-        };
-        return;
-      },
+    if !self.has_peer( ) {
+      return;
     }
     
     // Read loop
     loop {
-      match read.read_line( ) {
-        // Line received, strip the newline and pass it back
-        Ok ( line ) => {
-          let mut pass = line.clone( );
-          pass.pop( );
-          match self.chan.send( connection::ConnEvent::Recv( pass ) ) {
-            Ok ( _ )  => {
-              try  = 0u8;
-              time = 5;
-            },
-            Err ( _ ) => {
-              debug::err( "irc reader send", "" );
-            },
-          };
-        },
-        
-        // An error occurred or the connection was severed
-        Err ( e )   => {
-          match e.kind {
-            io::IoErrorKind::EndOfFile => {
-              debug::oper( "reader connection closed (eof reported)" );
-              break;
-            },
-            _                          => {
-              debug::err( "irc reader", e.desc );
-              match e.detail {
-                Some( det ) => debug::err( "irc reader", det.as_slice( ) ),
-                None        => (),
-              };
-              try += 1u8;
-            }
-          }
-        }
-      }
+      try = match read.read_line( ) {
+        Ok ( line ) => self.handle_read_success( line, try ),
+        Err ( e )   => self.handle_read_failure( e, try ),
+      };
         
       // Fail automatically after 5 tries
-      if try > 5 {
-        debug::err( "irc reader", "read failed after 5 retries" );
-        break;
-      // If we encounter an error, try again after a period of time
-      } else if try > 0 {
-        debug::info( "irc read failed, retrying after some time..." );
-        io::timer::sleep( time::Duration::seconds( time ) );
-        time *= 2;
-      }
+      match self.get_next_try( try, time ) {
+        None       => break,
+        Some ( t ) => time = t,
+      };
     }
     
     debug::oper( "closing irc reader..." );
-    match self.chan.send( connection::ConnEvent::Abort( String::from_str( "irc reader closed" ) ) ) {
+    match self.chan.send( ConnEvent::Abort( String::from_str( "irc reader closed" ) ) ) {
       Ok ( _ )  => (),
       Err ( _ ) => debug::err( "closing irc reader", "" ),
     }
